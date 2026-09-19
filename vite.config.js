e9f9af09f6fc6cbfe6aaa4e8aea161d83d283e65
pavan-7282 <pavanbollam8688@gmail.com>
2026-09-19 15:08:44 +0530

    Fix Vite configuration for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/food-delivery-using-react/',
+
+  base: "/",
+
   optimizeDeps: {
     esbuildOptions: {
       loader: {
-        '.js': 'jsx',
+        ".js": "jsx",
       },
     },
   },
+
   esbuild: {
-    loader: 'jsx',
+    loader: "jsx",
     include: /src\/.*\.(js|jsx)$/,
     exclude: [],
   },
-}))
+});
